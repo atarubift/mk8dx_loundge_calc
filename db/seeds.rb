847cfccac8a32e3f498d89f5cdd6429ccf276ad5
atarubift <atarubift@gmail.com>
2023-11-20 21:32:49 +0900

    add: document

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# cup_path = 'doc/cup.txt'
+
+# File.foreach(Rails.root.join(cup_path)) do |line|
+#   TrackCategory.create(name: line.chomp)
+# end
+
+# track_path = 'doc/track.txt'
+
+# i = 1
+# j = 1
+# File.foreach(Rails.root.join(track_path)) do |line|
+#   if j == 5
+#     i += 1
+#     j = 1
+#   end
+#   Track.create(name: line.chomp, track_category_id: i)
+#   j += 1
+# end
+
+# mogi_path = 'doc/mogi_type.txt'
+
+# File.foreach(Rails.root.join(mogi_path)) do |line|
+#   MogiType.create(name: line.chomp)
+# end
