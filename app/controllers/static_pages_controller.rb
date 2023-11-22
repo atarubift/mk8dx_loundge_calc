8838218e6f25579937dad54f3b8f4513a8600c29
atarubift <atarubift@gmail.com>
2023-11-23 01:08:51 +0900

    add: total track ave

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   def home
     @mogis = Mogi.all
     @mogis_last = Mogi.order(created_at: :desc).limit(5)
+    @mogi_results = MogiResult.all
   end
 end
